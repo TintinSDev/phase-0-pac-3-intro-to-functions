faecdb35de52f7b3eb63276afd7d91827edb6bd9
Martin <mmaina290@gmail.com>
2023-09-13 20:55:26 +0300

    js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Follow along with the examples here
+/* Follow along with the examples here
+function sayHello() {
+    console.log("Hello, Martin!");
+}
+
+function sayHelloToIsa() {
+    console.log("Hello, Isa!");
+}
+function sayHelloToMark() {
+    console.log("Hello,Mark!")
+}
+
+sayHello();
+sayHelloToIsa();
+sayHelloToMark();
+
+function doSomething(thing) {
+    console.log("thing");
+}
+doSomething("anything");
+*/
+function remainder(num1, num2) {
+    return (num1 % num2);
+}
+console.log(remainder(100, 23));
